d471c4d30ae4ce221cb23ef04c21418b1cdff1ed
laoazhang <laoazhang@163.com>
2024-01-15 15:23:07 +0800

    游乐园、学习基本api开发，了解生命周期

--- applet小程序/code/wx-yly/app.js
+++ applet小程序/code/wx-yly/app.js
@@ -1,19 +1,30 @@
 // app.js
+// App函数执行后，在微信客户端注册小程序，注册之后会伴随着执行一些生命周期钩子函数
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+  // 1. 默认小程序启动会执行前两个
+  // 小程序启动时
+  onLaunch(res) {
+    // 全局只会触发1次
+    // res.scene 场景值作用：统计小程序打开方式 => 更有针对性的引流 => 创造价值
+    // 前端：调用后台接口计算数量
+    console.log('小程序启动了...',res);
   },
-  globalData: {
-    userInfo: null
+  // 小程序前台运行时 => 前台运行会执行
+  onShow(res) {
+    // 重复执行
+    console.log('小程序前台运行了...',res);
+  },
+  // 小程序后台运行时 => 后台运行会执行
+  onHide: function () {
+    // 重复执行
+    console.log('小程序后台运行了...');
+  },
+  // 执行错误时
+  onError: function (err) {
+    console.log('出错啦!!!');
+  },
+  // 冷启动（如扫码）打开小程序的页面不存在时
+  onPageNotFound: function () {
+    console.log('你找到页面去火星了？？？');
   }
 })
--- applet小程序/code/wx-yly/pages/card/card.js
+++ applet小程序/code/wx-yly/pages/card/card.js
@@ -1,66 +1,78 @@
 // pages/card/card.js
 Page({
-
   /**
-   * 页面的初始数据
+   * 小程序的页面在跳转路由后，被销毁了吗？ => 没有
+   * vue的页面在跳转路由后，被销毁了吗？ => 没有
+   * 如果想缓存vue组件？ => <keep-alive include="name1,name2"> 要缓存的组件 </keep-alive>
    */
-  data: {
+// 1. 前边3个在页面第一次加载的时候会执行一次
+// 页面加载时 vue=>created
+onLoad: function () {
+  // 只会触发1次 => 发送请求，获取后台数据
+  console.log('当前页面被加载了...');
+  console.log('wx提供的api：', wx);
+  wx.request({
+    url: 'http://5990367be1e4470011c46fa8.mockapi.io/meng/user', 
+    success (res) {
+      console.log(res)
+    },
+    fail(){
 
-  },
+    }
+  })
+},
+useApi(){
+  // wx.showToast({
+  //   title: '消息提示',
+  //   icon: 'error',
+  //   duration: 2000
+  // })
+  // wx.showModal({
+  //   title: '提示',
+  //   content: '这是一个模态弹窗',
+  //   success (res) {
+  //     if (res.confirm) {
+  //       console.log('用户点击确定')
+  //       wx.hideTabBar({
+  //         animation:true
+  //       })
+  //     } else if (res.cancel) {
+  //       wx.showTabBar({
+  //         animation:true
+  //       })
+  //       console.log('用户点击取消')
+  //     }
+  //   }
+  // })
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+  // 跳转
+  wx.navigateTo({
+    url: '/pages/test/test',
+  })
+  wx.setStorage({
+    key:'test',
+    data:[1,2,3]
+  },{
+    key:'demo',
+    data:true
+  })
 
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+},
 
-  },
+// 页面显示时（返回、tabBar切换、前台运行）
+onShow: function () {
+  // 重复触发
+  console.log('当前页面显示了...');
+},
+// vue=> mounted
+onReady: function () {
+  // 只会触发1次
+  console.log('当前页面初次渲染完毕了...');
+},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+// 页面隐藏时（跳转、tabBar切换、后台运行）
+onHide: function () {
+  console.log('当前页面不见了...');
+}
 })
