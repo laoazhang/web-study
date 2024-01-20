47438340ade1e9b3793c693f73fb0ddb70e7bb37
laoazhang <laoazhang@163.com>
2024-01-20 23:58:41 +0800

    黑马智慧园区基础项目搭建

---
+++ hmzs/src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import 'normalize.css/normalize.css'
+import ElementUI from 'element-ui'
+// 引入element-ui的样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入样式文件
+import '@/styles/index.scss' // global css
+
+// 引入App根组件
+import App from './App'
+
+// 引入Vuex
+import store from './store'
+
+// 引入Vue-Route
+import router from './router'
+
+// 注册svg
+import '@/icons'
+
+// 结果：把element-ui中导出的所有组件，注册成全局组件
+Vue.use(ElementUI)
+
+// 上线之后如果有警告，要不要展示
+Vue.config.productionTip = false
+
+// vue实例化
+// 注入 router 和store实例 方便在项目中使用 this.$router this.$store
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
