cd9740531095bf7b69f9255b459d9f1f81dc92c5
laoazhang <laoazhang@163.com>
2024-02-04 20:45:06 +0800

    园区产业分布一

--- hmzs-screen/src/views/BigScreenView.vue
+++ hmzs-screen/src/views/BigScreenView.vue
@@ -154,10 +154,35 @@ onMounted(async () => {
       </div>
       <div class="bar-chart" ref="barChart"></div>
     </div>
+    <!-- 园区产业分布 -->
+<div class="section-three">
+  <img class="img-header"
+    src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E4%BA%A7%E4%B8%9A%E5%88%86%E5%B8%83%402x.png"
+    alt="" />
+  <div class="pie-chart" ref="pieChart"></div>
+</div>
   </div>
 </template>
 
 <style>
+.section-two {
+    flex-basis: 35%;
+    .bar-chart {
+      width: 100%;
+      height: calc(100% - 90px);
+    }
+  }
+  .section-three {
+    flex-basis: 40%;
+
+    .pie-chart {
+      position: relative;
+      margin: 0 auto;
+      padding-bottom: 20px;
+      width: 80%;
+      height: calc(100% - 40px);
+    }
+  }
 .section-two {
   flex-basis: 35%;
   margin-top: 50px;
