e1f74096012a86310793716cc787bdaa3cbb40ca
laoazhang <laoazhang@163.com>
2024-02-05 18:05:52 +0800

    园区产业分布

--- hmzs-screen/src/views/BigScreenView.vue
+++ hmzs-screen/src/views/BigScreenView.vue
@@ -85,10 +85,56 @@ const initBarChart = () => {
 
 }
 
+// 渲染园区产业分布图表
+const pieChart = ref(null)
+const initPieChart = () => {
+  const { parkIndustry } = parkInfo.value
+  const pieOption = {
+    color: [
+      '#00B2FF', '#2CF2FF', '#892CFF', '#FF624D', '#FFCF54', '#86ECA2'],
+    legend: {
+      itemGap: 20,
+      bottom: '0',
+      icon: 'rect',
+      itemHeight: 10, // 图例icon高度
+      itemWidth: 10, // 图例icon宽度
+      textStyle: {
+        color: '#c6d1db',
+      },
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    series: [
+      {
+        name: '园区产业分析',
+        type: 'pie',
+        radius: ['55%', '60%'], // 设置内圈与外圈的半径使其呈现为环形
+        center: ['50%', '40%'], // 圆心位置， 用于调整整个图的位置
+        tooltip: {
+          trigger: 'item',
+          formatter: (params) => {
+            return `${params.seriesName}</br><div style='display:flex;justify-content: space-between;'><div>${params.marker}${params.name}</div><div>${params.percent}%</div></div>`
+          }
+        },
+        label: {
+          show: false,
+          position: 'center',
+        },
+        data: parkIndustry,
+      },
+    ],
+
+  }
+  const myPieChart = echarts.init(pieChart.value)
+  myPieChart.setOption(pieOption)
+}
+
 
 onMounted(async () => {
   await getParkInfo()
   await initBarChart()
+  await initPieChart()
 })
 
 </script>
@@ -155,34 +201,37 @@ onMounted(async () => {
       <div class="bar-chart" ref="barChart"></div>
     </div>
     <!-- 园区产业分布 -->
-<div class="section-three">
-  <img class="img-header"
-    src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E4%BA%A7%E4%B8%9A%E5%88%86%E5%B8%83%402x.png"
-    alt="" />
-  <div class="pie-chart" ref="pieChart"></div>
-</div>
+    <div class="section-three">
+      <img class="img-header"
+        src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E4%BA%A7%E4%B8%9A%E5%88%86%E5%B8%83%402x.png"
+        alt="" />
+      <div class="pie-chart" ref="pieChart"></div>
+    </div>
   </div>
 </template>
 
 <style>
 .section-two {
-    flex-basis: 35%;
-    .bar-chart {
-      width: 100%;
-      height: calc(100% - 90px);
-    }
-  }
-  .section-three {
-    flex-basis: 40%;
+  flex-basis: 35%;
 
-    .pie-chart {
-      position: relative;
-      margin: 0 auto;
-      padding-bottom: 20px;
-      width: 80%;
-      height: calc(100% - 40px);
-    }
+  .bar-chart {
+    width: 100%;
+    height: calc(100% - 90px);
   }
+}
+
+.section-three {
+  flex-basis: 40%;
+
+  .pie-chart {
+    position: relative;
+    margin: 0 auto;
+    padding-bottom: 20px;
+    width: 80%;
+    height: calc(100% - 40px);
+  }
+}
+
 .section-two {
   flex-basis: 35%;
   margin-top: 50px;
