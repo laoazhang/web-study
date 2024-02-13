91d2dab4d8aa62234c413eadc5424fec6f33a99b
laoazhang <laoazhang@163.com>
2024-02-13 19:40:25 +0800

    员工列表渲染、新增员工、删除员工、员工状态适配

---
+++ hmzs/src/api/employee.js
@@ -0,0 +1,31 @@
+import request from '@/utils/request'
+
+/**
+ * 获取员工列表
+ * @param { page, pageSize, name} params
+ * @returns
+ */
+export const getEmployeeListAPI = (params) => request({ url: '/park/sys/user', params })
+
+/**
+ * 添加员工
+ * @returns
+ */
+export function createEmployeeAPI(data) {
+  return request({
+    url: `/park/sys/user`,
+    method: 'POST',
+    data
+  })
+}
+
+/**
+ * 删除员工
+ * @returns
+ */
+export function delEmployeeAPI(id) {
+  return request({
+    url: `/park/sys/user/${id}`,
+    method: 'DELETE'
+  })
+}
