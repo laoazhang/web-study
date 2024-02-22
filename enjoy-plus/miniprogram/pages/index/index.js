f2e06e398ae59e6b0a7bb6a948def277986d876c
laoazhang <laoazhang@163.com>
2024-02-22 11:31:29 +0800

    公告列表渲染，详情展示

--- enjoy-plus/miniprogram/pages/index/index.js
+++ enjoy-plus/miniprogram/pages/index/index.js
@@ -1,5 +1,15 @@
 Page({
   onLoad() {
-    wx.utils.toast('Hello World!')
-  }
+    this.getNotices()
+  },
+  // 获取公告列表
+  async getNotices() {
+    const { code, data: notices } = await wx.http({
+      url: '/announcement',
+      method: 'GET'
+    })
+    console.log('公告列表数据', code, notices);
+    // 更新页面数据
+    this.setData({ notices })
+  },
 })
--- enjoy-plus/miniprogram/pages/notify/index.js
+++ enjoy-plus/miniprogram/pages/notify/index.js
@@ -5,14 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    noticeDetail: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
+  onLoad({ id }) {
+    this.getNoticesDetail(id)
+  },
 
+  // 获取公告详情数据
+  async getNoticesDetail(id) {
+    // 检测id是否存在
+    if (typeof id === undefined) return
+    console.log('id', id);
+    const { code, data: noticeDetail } = await wx.http.get('/announcement/' + id)
+    console.log('公告详情数据', code, noticeDetail);
+    //渲染数据
+    this.setData({ noticeDetail })
   },
 
   /**
